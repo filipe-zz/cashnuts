7ce2e06c9c335a4220eb2d29d2d5979733095a5d
Filipe Coimbra <filipe@MacBook.local>
2008-03-15 22:38:10 -0300

    Implemeting States tests and implemeting its model to pass those tests

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,6 @@
 class City < ActiveRecord::Base
+  belongs_to :state
+  
+  validates_presence_of :name
+  validates_presence_of :state_id
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,14 +1,23 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe City do
+  fixtures :states
+  
   before(:each) do
     @city = City.new
   end
   
-  it "should have a name"
-  it "should have a state"
+  it "should have a name" do
+    @city.should have(1).error_on(:name)
+  end
+  
+  it "should have a state" do
+    @city.should have(1).error_on(:state_id)
+  end
   
   it "should be valid" do
+    @city.name = "Maceio"
+    @city.state = states(:alagoas)
     @city.should be_valid
   end
 end
