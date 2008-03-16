682294360562d878b9ede4537091a7529ff7c669
Filipe Coimbra <filipe@MacBook.local>
2008-03-15 21:37:56 -0300

    Defining tests for State model

--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -4,6 +4,10 @@ describe State do
   before(:each) do
     @state = State.new
   end
+  
+  it "should have a name"
+  it "should have an abbreviation"
+  it "should have an abbreviation with 2 characters"
 
   it "should be valid" do
     @state.should be_valid
