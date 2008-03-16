226882e21db25a23ab092a0d6fe8d3979a616e2f
Filipe Coimbra <filipe@MacBook.local>
2008-03-15 22:14:38 -0300

    Implemeting State to pass tests

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,2 +1,5 @@
 class State < ActiveRecord::Base
+  validates_presence_of :name
+  
+  validates_length_of :abbreviation, :is => 2
 end
--- spec/models/state_spec.rb
+++ spec/models/state_spec.rb
@@ -1,35 +1,41 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
-describe State do
-  before(:each) do
-    @valid_state = {
+module StateSpecHelper
+  def valid_attr
+    {
       :name => "Alagoas",
       :abbreviation => "AL"
     }
-    
+  end
+end
+
+describe State do
+  include StateSpecHelper
+  
+  before(:each) do
     @state = State.new
   end
   
   it "should have a name" do
-    @state.attributes = @valid_state.except(:name)
+    @state.attributes = valid_attr.except(:name)
     @state.should have(1).error_on(:name)
   end
   
   it "should have an abbreviation" do
-    @state.attributes = @valid_state.except(:abbreviation)
+    @state.attributes = valid_attr.except(:abbreviation)
     @state.should have(1).error_on(:abbreviation)
   end
   
   it "should have an abbreviation with 2 characters" do
-    @state.attributes = @valid_state.except(:abbreviation)
+    @state.attributes = valid_attr.except(:abbreviation)
     @state.abbreviation = "A"
     @state.should have(1).error_on(:abbreviation)
-    @state.abbreviation = "ALA"
+    @state.abbreviation = "A"
     @state.should have(1).error_on(:abbreviation)
   end
 
   it "should be valid" do
-    @state.attributes = @valid_state
+    @state.attributes = valid_attr
     @state.should be_valid
   end
 end
